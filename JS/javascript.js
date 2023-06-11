5f5ad1e00508483c70fc0f942bf8a36fde7767d8
BalabaSerhii <balabaserhii@gmail.com>
2023-06-11 20:06:21 +0200

    next

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,10 +1,40 @@
-const scores = [1, 2, 3, 4, 5];
-const res = [31, 52, 364, 43, 52];
 
-const test = res.concat(scores);
+const btnEl = document.querySelector("#btn");
+btnEl.style.textTransform = "uppercase"
+
+console.log(btnEl.value);
 
 
-console.log(test);
+btnEl.addEventListener("click", () => 
+	btnEl.style.color = "green");
 
+
+
+
+
+
+
+
+// const nonOk = "Недостаточно средств";
+// const okey = "Операция успешная";
+// const bal = "Нужно ввести что-то больше 0";
+
+
+// const test = function withdraw(amount, balance) {
+// 	if (amount === 0) {
+// 		console.log(bal);
+// 		return;
+// 	}
+	
+// 	if (amount > balance) {
+// 		console.log(nonOk);
+// 		return;
+// 	}
+
+// 	console.log("okey");
+	
+// }
+
+// test(1330, 103);
 
 
