0377738575a8e2a46ef738b036b40fe680fad71e
BalabaSerhii <balabaserhii@gmail.com>
2023-06-26 21:16:23 +0200

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -50,16 +50,73 @@
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
 // / Заменить регистр букв на противоположный. Второй вариант норм
-const string = 'JavaScript';
-const stringNew = string.split('');
-let inverted = '';
+// const string = 'JavaScript';
+// const stringNew = string.split('');
+// let inverted = '';
 
+// for (const stringAll of stringNew) {
+	// 	const test = stringAll === stringAll.toLowerCase();
+	// 	inverted += test ? stringAll.toUpperCase() : stringAll.toLowerCase();
+	// }
+	// console.log(inverted)
 
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+// Делаем slug в URL из названия статьи.
+// -Нормализируем строку
+// -Разбиваем по словам
+// -Сшиваем в строку с разделителями
 
+// const title = 'Top 10 benefists of React Framework';
+// const normolize = title.toLowerCase().split(' ').join('-')
+// console.log("~  normolize:", normolize)
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//  Скрипт который считает сумму элементов двух массивов.
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+// let total = 0;
 
-    
-  
-    
+// const sums = array1.concat(array2);
+// for (const sum of sums) {
+	// 	total += sum;
+	// }
+	// console.log(total);
+	/////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////
+	// Работаем с коллекцией карточек в trello
+	// -метод splice()
+	// 	- удалить
+	// 	- добавить
+	// 	- обновить
+	
+	// const cards = [
+		// 	'Карточка-1',
+		// 	'Карточка-2',
+		// 	'Карточка-3',
+		// 	'Карточка-4',
+		// 	'Карточка-5',
+		// ];
+		// console.table(cards)
+		// удаление
+		// const cardToRemove = 'Карточка-3';
+		// const index = cards.indexOf(cardToRemove);
+		// cards.splice(index, 1);
+		// console.table(cards);
+		
+		// добавление
+		// const cardToInsert = 'Карточка-6';
+		// cards.splice(1, 0, cardToInsert);
+		// console.table(cards);
+		
+		// обновление
+		// const cardToUpdate = 'Эта штука заменила(обновила) 2ю карточку';
+		// cards.splice(1, 1, cardToUpdate);
+		// console.table(cards);
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
+		
+		
     
     
     
