ee152fa8efa27f48136ad5b75a6ab577e8128fcb
BalabaSerhii <balabaserhii@gmail.com>
2023-08-11 22:26:36 +0200

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -292,22 +292,75 @@
 // }
 // console.log("colorPikerData:", colorPikerData)
 
+// Change c
 
 
 
 
 
 
-const students = [
-  { name: "Манго", courses: ["математика", "физика"] },
-  { name: "Поли", courses: ["информатика", "математика"] },
-  { name: "Киви", courses: ["физика", "биология"] },
-];
 
-const allCourses = students.flatMap(student => student.courses);
-// ['математика', 'физика', 'информатика', 'математика', 'физика', 'биология'];
 
-const uniqueCourses = allCourses.filter(
-  (course, index, array) => array.indexOf(course) === index
-  );
-  console.log("~  uniqueCourses:", uniqueCourses.allCourses)
+
+
+
+
+
+
+
+
+
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Change code below this line
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+	blacklistedEmails = [];
+  constructor({ email, accessLevel }) {
+    super(email);
+	  this.accessLevel = accessLevel;
+	  
+  }
+	blacklist(email) {
+		this.blacklistedEmails.push(email);
+	}
+
+	isBlacklisted(email) {
+		return this.blacklistedEmails.includes(email);
+		
+	}
+	// Change code above this line
+	
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
